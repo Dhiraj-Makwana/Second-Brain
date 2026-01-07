6b1c01b61223d1e12c6be2477805de19bf64ff5f
Dhiraj-Makwana <dhirajmakwana33912@gmail.com>
2026-01-07 23:37:24 +0530

    add signin endpoint with jwt auth token

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -3,10 +3,21 @@ import { UserModel } from "./db"
 import mongoose from "mongoose"
 import z from "zod"
 import bcrypt from "bcrypt"
+import cors from "cors"
+import jwt from "jsonwebtoken"
+import dotenv from "dotenv"
+
+const JWT_SECRET = process.env.JWT_PASSWORD;
+
+if (!JWT_SECRET) {
+  throw new Error("JWT SECRET is not defined in environment variables");
+}
 
 const app = express()
 app.use(express.json())
+app.use(cors())
 
+/*----------SignUp_&_SignIn----------*/
 const SignUpSchema = z.object({
   username: z
     .string()
@@ -56,6 +67,40 @@ app.post("/api/v1/signup", async (req, res) => {
       error: "User already exist:( Please Sign in",
     });
   }
+});
+
+app.post("/api/v1/signin", async (req, res) => {
+  const result = SignUpSchema.safeParse(req.body);
+
+  if (!result.success) {
+    return res.status(400).json({
+      errors: result.error,
+    });
+  }
+
+  const { username, password } = result.data;
+
+  const existingUser = await UserModel.findOne({ username });
+
+  if (!existingUser) {
+    return res.status(403).json({
+      message: "Incorrect Credentials",
+    });
+  }
+  const isPasswordCorrect = await bcrypt.compare(
+    password,
+    existingUser.password
+  );
+
+  if (!isPasswordCorrect) {
+    res.status(403).json({
+      message: "Incorrect Credentials",
+    });
+  }
+
+  const token = jwt.sign({ id: existingUser._id }, JWT_SECRET);
+
+  return res.json({ token });
 });
 
 app.listen(3000)
